6a8051748893944def001b6a514b501bd1ac6cd4
lzhoubos <lzhou.bos@gmail.com>
2014-11-08 15:51:12 -0500

    add_data.sh

--- src/add_data.sh
+++ src/add_data.sh
@@ -1,6 +1,11 @@
 DRUG=$(python assign_drug.py $2)
 DEST=data/$1/$1-$DRUG.dat
-echo cp $2 $DEST
+mkdir -p data/$1
+cp $2 $DEST
+git add $DEST
+MSG="Add file $DEST"
+git commit -m "'$MSG'" 
+echo "New file added to the repo:$DEST"
 
 #bash add_data.sh 2014-11-07 inflammtion_1.dat
 #cp inflammtion_1.dat data/2014-11-07/2014-11-07-tylenol.dat
